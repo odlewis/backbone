b85443cec2610febd3dcbff3c89f5abe185ad7da
Omar Lewis <odlewis@prodigy.net>
2018-11-22 12:57:20 -0500

    using

--- js/myJs.js
+++ js/myJs.js
@@ -1,3 +1,4 @@
+ $(document).ready(function() {
 // Define Constructor Object 
 Book = Backbone.Model.extend({
     initialize: function(){
@@ -9,7 +10,10 @@ Book = Backbone.Model.extend({
     {
         name : "The Return of Simple",
         author: "Lanston Hughes",
-        publishDate: "January 15, 1967"
+        publishDate: "January 15, 1967",
+        data: [{va11:"hello"},
+               {val2:"world"}]
+
     }
 });
 
@@ -18,44 +22,91 @@ Book = Backbone.Model.extend({
 //console.log(myBook.get('name'));
 //console.log('The author is now '+myBook.get('author'));
 
-newBook = new Book({name: "Beginning Backbone", author: "James Sugrue"});
-console.log("Book: "+ newBook.get('name')+ " Author: " + newBook.get('author'));
+newBook = new Book({
+    name: "Beginning Backbone", 
+    author: "James Sugrue",
+    publishDate: "September 1, 2008"
+});
+
+console.log("newBook: "+ newBook.get('name')+ " Author: " + newBook.get('author'));
 
 LibraryView = Backbone.View.extend({
     initialize:function(){ 
         console.log("Library view created...");
-        this.render();
+        $("#cream").select2();
+        var stateRendered =false;
     },
 
     events:{
-        'click' : 'theView'
+        'click' : 'theView',
+        'select': 'theChamp', 
+        'change': 'onChange'
     },
         
     
     render:function(){
         console.log("Rendering...");
-        this.$el.html('Book Name: '+this.model.get('name'));
+        
+        if(!this.stateRendered){
         this.$el.append('<p>This is it</p>');
         this.$el.append('<button type="button" class="btn btn-primary">Submit</button>');
+        this.$el.append('<br><select id= "hey" name="state"><option value="blank"></option> <option value="OH">Ohio</option><option value="MD">Maryland</option><option value="WV">West Virginia</option><option value="FL">Florida</option><option value="TX">Texas</option>');
+            
 
+            
+        this.$el.append('<div class="foo"></div>');
+        $('#hey').append('.foo').select2();
+        this.stateRendered = true;
+    }
+//    $('.myLibraryViewSection').select2(this.data);
+/*    $('.myLibraryViewSection').select2(
+        {placeholder:"Pick a state", 
+        data: [
+            {id: 0, text: ""},
+            {id: 1, text: "Alabama"},
+            {id: 2, text: "Virginia"},
+            {id: 3, text: "Michigan"}],
+        allowclear: true
+        } 
+    );
+*/ 
+//    this.$el.append('<select class="tical"></select>');
+//    $("#wobble").select2().trigger('select');
+//    $("#wobble").select2();
+        
+
+      
         return this;
     },
     theView: function(e){
- 
-        
-        
-        alert("What's happening.");
-        
-//        this.$el.append('<p>This is it '+myView.get('name')+'</p>');
+//        alert("What's happening.");        
+        this.$el.append('<p>This is it '+this.model.get('name')+'</p>');
        console.log("something clicked...");
+       this.render();
 
+
+    },
+    theChamp: function(e)
+    {
+        console.log('Testing 1 2 3 select2');    
+    },
+    onChange: function(e)
+    {
+        console.log('Change it up');
     }
 });
 
 
 console.log('Hello');
-        goodBook = new Book({name: "I Am Not Your Negro", author: "James Baldwin"});
+goodBook = new Book({
+    name: "I Am Not Your Negro", 
+    author: "James Baldwin",
+    publishDate: "Unknown"
+});
     
-        // Use el by reference an existing DOM element or by creating a new one.
-        myView = new LibraryView({model:goodBook,
-                         el: '#myLibraryViewSection'});
+// Use el by reference an existing DOM element or by creating a new one.
+myView = new LibraryView({
+    model:goodBook,
+    el: '.myLibraryViewSection'
+});
+ })
